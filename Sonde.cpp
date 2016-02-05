97fa637d317d111c047ae38b8e46738c1984efde
Tolcantor <guiblinmaxime@gmail.com>
2016-02-05 15:00:00 +0100

    Mise à jour de la méthode Update

--- Sonde.cpp
+++ Sonde.cpp
@@ -1,9 +1,12 @@
 #include "Sonde.h"
 #include "Astre.h"
+#include <math.h>
+#define G 1
 
 Sonde::Sonde(Astre *a, int nbA)
 {
-    //ctor
+    X.SetValue(0,0,100);
+    t=0;
 }
 
 Sonde::~Sonde()
@@ -11,10 +14,30 @@ Sonde::~Sonde()
     //dtor
 }
 
-Sonde::EDP(double t, double* X, double* dx){
+void Sonde::Update(double h)
+{
+    Matrice Xnew(2,1);
+    Matrice C(2,2);
+    Matrice* Xi;
+    Matrice D(2,2);
+    D.SetValue(0,0,1);
+    D.SetValue(0,1,h);
+    D.SetValue(1,0,1);
 
-}
+    for(int i=0; i<GetnbAstres(); i++)
+    {
+        astre[i].Update(t);
+        Xi = astre[i].GetX();
+        d.SetValue(i,0,sqrt(pow((*Xi).GetValue(0,0)-X.GetValue(0,0),2) + pow((*Xi).GetValue(0,1)-X.GetValue(0,1),2)));
+    }
 
-Sonde::Euler(double *X, double t, double dt ){
-
+    for(int i=0; i<GetnbAstres(); i++)
+    {
+        C.SetValue(2,1,h*G*(astre[i].Getmass())/(pow(d.GetValue(i,0),3)));
+        Xi = astre[i].GetX();
+        Xnew = Xnew + C*(*Xi-X);
+    }
+    Xnew = Xnew + D*X;
+    X=Xnew;
+    t=t+h;
 }
