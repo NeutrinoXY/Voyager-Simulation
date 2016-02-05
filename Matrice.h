362aa872f24a92374051e92ebc7593dfffe343cc
Tolcantor <guiblinmaxime@gmail.com>
2016-02-05 17:44:01 +0100

    Update Matrice.h

--- Matrice.h
+++ Matrice.h
@@ -8,7 +8,7 @@ class Matrice
         Matrice(int a, int b);
         virtual ~Matrice();
         double GetValue(int a,int b) { return matrix[a][b]; }
-        void GetSize(int& a, int& b) {b=nl;a=nc;}
+        void GetSize(int& a, int& b) {a=nl;b=nc;}
         void Set(double** val) { matrix = val; }
         void SetValue(int a, int b, double x) {matrix[a][b]=x;}
 
@@ -25,3 +25,4 @@ Matrice operator+(Matrice A, Matrice B);
 Matrice operator-(Matrice A, Matrice B);
 
 #endif // MATRICE_H
+
