dd398601f7bebfca805a6cb24e03ed34e9523362
Watashiku <adelbeque@hotmail.fr>
2016-02-05 20:34:03 +0100

    Update main.cpp
    Pour l'affichage graphique il faut se mettre en plein ecran, puis appuyer sur entrée.
    Si l'ecran est trop petit diminuez vos chiffres dans les if

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
+#include <stdlib.h>
+
 #include "Sonde.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    //cout << "Hello world!" << endl;
     Matrice A(2,2), B(2,1), C(2,1);
     Astre* astres = new Astre[1];
-    cout<<"lol"<<endl;
+    //cout<<"lol"<<endl;
     Sonde sonde(astres, 1);
-    cout<<sonde.Gett()<<endl;
-    cout<<sonde.GetnbAstres()<<endl;
+    //cout<<sonde.Gett()<<endl;
+    //cout<<sonde.GetnbAstres()<<endl;
 
     A.SetValue(0,0,1);    A.SetValue(0,1,4);
     A.SetValue(1,0,1);    A.SetValue(1,1,1);
@@ -21,22 +23,72 @@ int main()
 
     C = A*B;
 
-    cout << "A : " << endl << A.GetValue(0,0) << " " << A.GetValue(0,1) << endl;
-    cout << A.GetValue(1,0) << " " << A.GetValue(1,1) << endl << endl;
-    cout << "B : " << endl << B.GetValue(0,0) << endl << B.GetValue(1,0) << endl << endl;
-    cout << "C : " << endl << C.GetValue(0,0) << " " << C.GetValue(1,0) << endl << endl;
+    //cout << "A : " << endl << A.GetValue(0,0) << " " << A.GetValue(0,1) << endl;
+    //cout << A.GetValue(1,0) << " " << A.GetValue(1,1) << endl << endl;
+    //cout << "B : " << endl << B.GetValue(0,0) << endl << B.GetValue(1,0) << endl << endl;
+    //cout << "C : " << endl << C.GetValue(0,0) << " " << C.GetValue(1,0) << endl << endl;
 
-    for(int j=0; j<2500; j++)
+    /*/for(int t=0; t<2500; t++)
     {
-
+        //system("cls");
         cout << "O"<<endl;
         Matrice** d;
         d = sonde.Getd();
-        for (int i=0; i<2*(**d).GetValue(0,0); i++)
+        for (int i=0; i<(**d).GetValue(0,0)/2; i++)
             cout << " ";
 
-        cout << "(°;...;°)Maxim(sans e)GAY" << endl;
+        cout << "(°;...;°)" << endl;
         sonde.Update(0.001);
+    }/*/
+    for(int t=0; t<2500; t++)
+    {
+        system("cls");
+        cout << 32+(int)sonde.GetY(0,0) << endl << 113+(int)sonde.GetX(0,0) << endl;
+        for(int i=0; i<61; i++)
+        {
+            if (i==32)
+            {
+                if(i==32+(int)sonde.GetY(0,0)/10)
+                {
+                    for(int j=0; j<237; j++)
+                    {
+                        if (j==113+(int)sonde.GetX(0,0))
+                            cout << "X";
+                        else if (j==113)
+                            cout << "O";
+                        else
+                            cout << "_";
+                    }
+                }
+                else
+                    for(int j=0; j<236; j++)
+                    {
+                        if (j==113)
+                            cout << "O";
+                        else
+                            cout << "_";
+                    }
+            }
+            else if (i==32+(int)sonde.GetY(0,0)/10)
+            {
+                cout << endl;
+                for(int j=0; j<236; j++)
+                {
+                    if (j==113+(int)sonde.GetX(0,0))
+                        cout << "X";
+                    else
+                        cout << "_";
+                }
+            }
+            else
+            {
+                cout << endl;
+                for(int j=0; j<236; j++)
+                    cout << "_";
+            }
+        }
+    sonde.Update(0.1);
+    cin.ignore(); // Pause
     }
 
     return 0;
