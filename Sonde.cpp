90ddab06e443f82f672424e8ae7d73c2f0937e6f
Watashiku <adelbeque@hotmail.fr>
2016-02-05 20:32:40 +0100

    Update Sonde.cpp

--- Sonde.cpp
+++ Sonde.cpp
@@ -9,10 +9,12 @@ using namespace std;
 Sonde::Sonde(Astre *a, int nbA)
 {
     astre=a;
-    X.SetValue(0,0,100);
+    X.SetValue(0,0,20);
+    Y.SetValue(0,0,-40);
+    X.SetValue(0,1,40000000000000000);
+    Y.SetValue(0,1,0);
     t=0;
     nbAstres=nbA;
-    Matrice x(nbA, 1);
     d = new Matrice(nbA,1);
 }
 
@@ -24,10 +26,11 @@ Sonde::~Sonde()
 void Sonde::Update(double h)
 {
     Matrice Xnew(2,1);
+    Matrice Ynew(2,1);
     Matrice C(2,2);
     Matrice* Xi;
+    Matrice* Yi;
     Matrice D(2,2);
-    Matrice Y(2,1);
     D.SetValue(0,0,1);
     D.SetValue(0,1,h);
     D.SetValue(1,1,1);
@@ -36,9 +39,11 @@ void Sonde::Update(double h)
     {
         astre[i].Update(t);
         Xi = astre[i].GetX();
-        (*d).SetValue(i,0,sqrt(pow((*Xi).GetValue(0,0)-X.GetValue(0,0),2) + pow((*Xi).GetValue(0,1)-X.GetValue(0,1),2)));
-        cout<<"d="<<sqrt(pow((*Xi).GetValue(0,0)-X.GetValue(0,0),2) + pow((*Xi).GetValue(0,1)-X.GetValue(0,1),2))<<endl;
-        cout<<"x="<<X.GetValue(0,0)<<endl;
+        Yi = astre[i].GetY();
+        (*d).SetValue(i,0,sqrt(pow((*Xi).GetValue(0,0)-X.GetValue(0,0),2) + pow((*Yi).GetValue(0,0)-Y.GetValue(0,0),2)));
+        //cout<<"d="<<sqrt(pow((*Xi).GetValue(0,0)-X.GetValue(0,0),2) + pow((*Yi).GetValue(0,0)-Y.GetValue(0,0),2))<<endl;
+        //cout<<"x="<<X.GetValue(0,0)<<endl;
+        //cout<<"y="<<Y.GetValue(0,0)<<endl;
     }
 
     for(int i=0; i<GetnbAstres(); i++)
@@ -49,13 +54,18 @@ void Sonde::Update(double h)
         //cout<<"HIHBIH"<<h*G*(astre[i].Getmass())/(pow((*d).GetValue(i,0),3))<<endl;
         //cout<<"C="<<C.GetValue(1,0)<<endl;
         Xi = astre[i].GetX();
-        Y = *Xi-X;
-        Xnew = Xnew + (C*Y);
+        Yi = astre[i].GetY();
+
+        Xnew = Xnew + (C*(*Xi-X));
+        Ynew = Ynew + (C*(*Yi-Y));
 
         //cout<<"AZAZSQZSAZA"<<(C*Y).GetValue(0,1)<<endl;
     }
     Xnew = Xnew + D*X;
-    cout<<"Xnew="<<Xnew.GetValue(1,0)<<endl;
+    Ynew = Ynew + D*Y;
+    //cout<<"Xnew="<<Xnew.GetValue(1,0)<<endl;
+    //cout<<"Ynew="<<Ynew.GetValue(1,0)<<endl;
     X=Xnew;
+    Y=Ynew;
     t=t+h;
 }
