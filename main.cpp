68d11c7445b329d033a29a81ce68c9d3567ebe3d
Watashiku <adelbeque@hotmail.fr>
2016-02-07 01:43:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,8 +10,11 @@ int main()
     //cout << "Hello world!" << endl;
     Matrice A(2,2), B(2,1), C(2,1);
     Astre* astres = new Astre[1];
+
+
     //cout<<"lol"<<endl;
     Sonde sonde(astres, 1);
+
     //cout<<sonde.Gett()<<endl;
     //cout<<sonde.GetnbAstres()<<endl;
 
@@ -22,6 +25,7 @@ int main()
     B.SetValue(1,0,1);   /* B.SetValue(1,1,0);*/
 
     C = A*B;
+
 
     //cout << "A : " << endl << A.GetValue(0,0) << " " << A.GetValue(0,1) << endl;
     //cout << A.GetValue(1,0) << " " << A.GetValue(1,1) << endl << endl;
@@ -40,56 +44,66 @@ int main()
         cout << "(°;...;°)" << endl;
         sonde.Update(0.001);
     }/*/
-    for(int t=0; t<2500000; t++)
+    for(int t=0; t<250000000; t++)
     {
-        system("cls");
-        cout << sonde.GetX(0,0) << "   " << sonde.GetX(1,0) << endl << sonde.GetY(0,0) << "   " << sonde.GetY(1,0) << endl;
-        for(int i=0; i<60; i++)
+        if (!(t%1000))
         {
-            if (i==32)
+            system("cls");
+            cout << sonde.GetX(0,0) << "   " << sonde.GetX(1,0) << endl << sonde.GetY(0,0) << "   " << sonde.GetY(1,0) << endl;
+            for(int i=0; i<60; i++)
             {
-                if(i==32+(int)sonde.GetY(0,0)/10)
+                if (i==32)
                 {
-                    for(int j=0; j<237; j++)
+                    if(i==32+(int)sonde.GetY(0,0)/2)
                     {
-                        if (j==113+(int)sonde.GetX(0,0))
+                        for(int j=0; j<237; j++)
+                        {
+                            if (j==118+(int)sonde.GetX(0,0))
+                                cout << "X";
+                            else if (j==118)
+                                cout << "O";
+                            else if (j > 118 && j > 118+(int)sonde.GetX(0,0))
+                            {
+                                cout << endl;
+                                j = 237;
+                            }
+                            else
+                                cout << " ";
+                        }
+                    }
+                    else
+                        for(int j=0; j<236; j++)
+                        {
+                            if (j==118)
+                            {
+                                cout << "O" << endl;
+                            }
+                            else
+                                cout << " ";
+                        }
+                }
+                else if (i==32+(int)sonde.GetY(0,0)/2)
+                {
+                    cout << endl;
+                    for(int j=0; j<236; j++)
+                    {
+                        if (j==118+(int)sonde.GetX(0,0))
                             cout << "X";
-                        else if (j==113)
-                            cout << "O";
                         else
                             cout << " ";
                     }
                 }
                 else
-                    for(int j=0; j<236; j++)
-                    {
-                        if (j==113)
-                            cout << "O";
-                        else
-                            cout << " ";
-                    }
-            }
-            else if (i==32+(int)sonde.GetY(0,0)/10)
-            {
-                cout << endl;
-                for(int j=0; j<236; j++)
                 {
-                    if (j==113+(int)sonde.GetX(0,0))
-                        cout << "X";
-                    else
+                    cout << endl;
+                    /*/for(int j=0; j<236; j++)
                         cout << " ";
+                    /*/
                 }
             }
-            else
-            {
-                cout << endl;
-                /*/for(int j=0; j<236; j++)
-                    cout << " ";
-                /*/
-            }
         }
-    sonde.Update(0.01);
-    if (!(t%1000))
+    sonde.Update(0.0001);
+    if (!(t%1000000000000))
         cin.ignore(); // Pause
     }
 
