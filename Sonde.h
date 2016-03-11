d8bdfebc995e9d071d536e64ee32558ad1e1baca
Tolcantor <guiblinmaxime@gmail.com>
2016-03-11 10:52:12 +0100

    Updated from Test-2D

--- Sonde.h
+++ Sonde.h
@@ -15,16 +15,15 @@ class Sonde
         double Gett() { return t; }
         Matrice** Getd() {return &d;}
         void Update(double h);
+        double GetX(int i,int j) {return X.GetValue(i,j);};
+        double GetY(int i,int j) {return Y.GetValue(i,j);};
     protected:
     private:
         int nbAstres;
         Astre* astre;
         double t;
         Matrice* d;
-        Matrice X = Matrice(2,1);
+        Matrice X = Matrice(2,1);//On utilise X[0] pour la position et X[1] pour la vitesse selon Ox
         Matrice Y = Matrice(2,1);
-};
-
+};    
 #endif // SONDE_H
-
-
