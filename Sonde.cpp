3341b57a6fa2a0052cbdbeb439ba9d841258ac0d
Watashiku <adelbeque@hotmail.fr>
2016-05-14 12:26:04 +0200

    Update Sonde.cpp

--- Sonde.cpp
+++ Sonde.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-Sonde::Sonde(Astre *a, int nbA)
+Sonde::Sonde(Astre *a, int nbA) : X(Matrice(2,1)), Y(Matrice(2,1)), d(Matrice(nbA,1))
 {
     astre=a;
     X.SetValue(0,0,1);
@@ -20,18 +20,19 @@ Sonde::Sonde(Astre *a, int nbA)
     Y.SetValue(1,0,9);
     t=0;
     nbAstres=nbA;
-    d = new Matrice(nbA,1);
 }
 
 Sonde::~Sonde()
 {
-    //delete d;
+
 }
 
 void Sonde::Update(double h)
 {
+
     Matrice Xnew(2,1);
     Matrice Ynew(2,1);
+    Matrice Temp(2,1);
     Matrice C(2,2);
     Matrice* Xi;
     Matrice* Yi;
@@ -48,26 +49,44 @@ void Sonde::Update(double h)
         Xi = astre[i].GetX();
         Yi = astre[i].GetY();
 
-        //Distance entre la sonde et l'astre i
-        (*d).SetValue(i,0,sqrt(pow((*Xi).GetValue(0,0)-X.GetValue(0,0),2) + pow((*Yi).GetValue(0,0)-Y.GetValue(0,0),2)));
-    }
 
+        //Distance entre la sonde et l'astre i
+        d.SetValue(i,0,sqrt(pow((*Xi).GetValue(0,0)-X.GetValue(0,0),2) + pow((*Yi).GetValue(0,0)-Y.GetValue(0,0),2)));
+    }
 
 
     for(int i=0; i<GetnbAstres(); i++)
     {
     	//Ceci est l'influence de l'astre i sur la sonde avec l'approximation de la methode d'Euler
-        C.SetValue(1,0,h*G*(astre[i].Getmass())/(pow((*d).GetValue(i,0),3))); // (C = pas*G*M/(R^3) )
+        C.SetValue(1,0,h*G*(astre[i].Getmass())/(pow(d.GetValue(i,0),3))); // (C = pas*G*M/(R^3) )
         Xi = astre[i].GetX();
         Yi = astre[i].GetY();
 
+
 		//On l'ajoute à la position actuelle et à l'influence des autres astres
-        Xnew = Xnew + (C*(*Xi-X));
-        Ynew = Ynew + (C*(*Yi-Y));
+
+        Temp = C*(*Xi);
+        Xnew.SetValue(0,0,Xnew.GetValue(0,0)+Temp.GetValue(0,0)-X.GetValue(0,0));
+        Xnew.SetValue(0,1,Xnew.GetValue(0,1)+Temp.GetValue(0,1)-X.GetValue(0,1));
+        Temp = C*(*Yi);
+        Ynew.SetValue(0,0,Ynew.GetValue(0,0)+Temp.GetValue(0,0)-Y.GetValue(0,0));
+        Ynew.SetValue(0,1,Ynew.GetValue(0,1)+Temp.GetValue(0,1)-Y.GetValue(0,1));
+
+
+        //Temp = Xnew + ((C*(*Xi)-X));
+        //Xnew = Temp;
+        //Temp = Ynew + ((C*(*Yi)-Y));
+        //Ynew = Temp;
+
     }
-    Xnew = Xnew + D*X;
-    Ynew = Ynew + D*Y;
-    X=Xnew;
-    Y=Ynew;
+    Temp = D*X;
+    X.SetValue(0,0,Xnew.GetValue(0,0)+Temp.GetValue(0,0));
+    X.SetValue(0,1,Xnew.GetValue(0,1)+Temp.GetValue(0,1));
+    Temp = D*Y;
+    Y.SetValue(0,0,Ynew.GetValue(0,0)+Temp.GetValue(0,0));
+    Y.SetValue(0,1,Ynew.GetValue(0,1)+Temp.GetValue(0,1));
+    cout<<"Bug?"<<endl;
     t=t+h;
+    cout<<"Bug?"<<endl;
+
 }
