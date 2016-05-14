134b4397df4d3bbfaea1e2a51f4c95470523c7ff
Watashiku <adelbeque@hotmail.fr>
2016-05-14 12:26:47 +0200

    Update Sonde.h

--- Sonde.h
+++ Sonde.h
@@ -9,21 +9,21 @@ class Sonde
     public:
         Sonde(Astre* a, int nbA);
         virtual ~Sonde();
-        int GetnbAstres() { return nbAstres; }
-        void SetnbAstres(int val) { nbAstres = val; }
-        Astre* Getastre() { return astre; }
-        double Gett() { return t; }
-        Matrice** Getd() {return &d;}
+        int GetnbAstres() const {return nbAstres;}
+        void SetnbAstres(int val) {nbAstres = val;}
+        Astre* Getastre() const {return astre;}
+        double Gett() const {return t;}
+        Matrice Getd() const {return d;}
         void Update(double h);
-        double GetX(int i,int j) {return X.GetValue(i,j);};
-        double GetY(int i,int j) {return Y.GetValue(i,j);};
+        double GetX(int i,int j) const {return X.GetValue(i,j);};
+        double GetY(int i,int j) const {return Y.GetValue(i,j);};
     protected:
     private:
         int nbAstres;
         Astre* astre;
         double t;
-        Matrice* d;
-        Matrice X = Matrice(2,1);//On utilise X[0] pour la position et X[1] pour la vitesse selon Ox
-        Matrice Y = Matrice(2,1);
-};    
+        Matrice d;
+        Matrice X;//On utilise X[0] pour la position et X[1] pour la vitesse selon Ox
+        Matrice Y;
+};
 #endif // SONDE_H
