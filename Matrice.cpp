59b11440135c0887f3a2face5d065479921a3d96
Watashiku <adelbeque@hotmail.fr>
2016-05-20 13:58:22 +0200

    Update Matrice.cpp

--- Matrice.cpp
+++ Matrice.cpp
@@ -5,85 +5,31 @@ using namespace std;
 
 Matrice::Matrice(int a, int b)
 {
+	//Allocation dynamique de la matrice
+    double** m = new double*[a];
+    for(int i=0; i<a ; i++)
+    {
+        m[i] = new double[b];
+
+        for(int j=0; j<b ; j++)
+        {
+            m[i][j]=0;
+        }
+    }
+
     //Mise a jour des variables internes à la matrice
+    matrix = m;
     nl = a;
     nc = b;
-	//Allocation dynamique de la matrice
-	matrix = new double*[nl];
-    matrix[0] = new double[nl*nc];
-    for(int i=1; i<nl ; i++){
-        matrix[i] = matrix[i-1] + nc;
-    }
-    for(int i=0; i<nl ; i++){
-        for(int j=0; j<nc ; j++){
-            matrix[i][j]=0;
-        }
-    }
-}
-
-Matrice::Matrice(const Matrice &M)
-{
-    //Mise a jour des variables internes à la matrice
-    M.GetSize(nl,nc);
-
-    if(this != &M){
-        for(int i=0; i<nl; i++){
-            delete[] matrix[i];
-        }
-        delete[] matrix;
-        //Allocation dynamique de la matrice
-        matrix = new double*[nl];
-        matrix[0] = new double[nl*nc];
-        for(int i=1; i<nl ; i++){
-            matrix[i] = matrix[i-1] + nc;
-        }
-        for(int i=0; i<nl ; i++){
-            for(int j=0; j<nc ; j++){
-                matrix[i][j]=M.GetValue(i,j);
-            }
-        }
-    }
-}
-
-Matrice::Matrice()
-{
-    nl = 0;
-    nc = 0;
-}
-
-Matrice& Matrice::operator=(const Matrice &B)
-{
-    if(this != &B){
-        for(int i=0; i<nl; i++){
-            delete[] matrix[i];
-        }
-    delete[] matrix;
-    }
-
-
-    B.GetSize(nl,nc);
-    for(int i=0; i<nl; i++){
-        for(int j=0; j<nc; j++){
-            matrix[i][j] = B.GetValue(i,j);
-        }
-    }
-
-    return *this;
 }
 
 Matrice::~Matrice()
 {
-    /*/
-    for(int i=0; i<nl; i++){
-        delete[] matrix[i];
-    }
-    delete[] matrix;
-    /*/
     //dtor
 }
 
 //Déclaration de l'opération * entre les matrices
-Matrice operator*(const Matrice &A, const Matrice &B)
+Matrice operator*(Matrice A, Matrice B)
 {
     int nlA, ncA, nlB, ncB;
     A.GetSize(nlA, ncA);
@@ -97,18 +43,27 @@ Matrice operator*(const Matrice &A, const Matrice &B)
     }
     else
     {
+        double** m = new double*[nlA];
+
+        for(int i=0; i<nlA ; i++)
+        {
+            m[i] = new double[ncB];
+            for (int j=0; j<ncB; j++)
+                m[i][j] = 0;
+        }
+
         for(int i=0; i<nlA ; i++)
             for(int j=0; j<ncB ; j++)
                 for(int k=0; k<ncA ; k++){
-                    result.SetValue(i,j,A.GetValue(i,k)*B.GetValue(k,j));
-                    }
+                    m[i][j]+=(A.GetValue(i,k))*(B.GetValue(k,j));}
+
+        result.Set(m);
         return result;
     }
 }
 
 
-
-Matrice operator+(const Matrice &A, const Matrice &B)
+Matrice operator+(Matrice A, Matrice B)
 {
     int nlA, ncA, nlB, ncB;
     A.GetSize(nlA, ncA);
@@ -141,8 +96,7 @@ Matrice operator+(const Matrice &A, const Matrice &B)
     }
 }
 
-
-Matrice operator-(const Matrice &A, const Matrice &B)
+Matrice operator-(Matrice A, Matrice B)
 {
     int nlA, ncA, nlB, ncB;
     A.GetSize(nlA, ncA);
