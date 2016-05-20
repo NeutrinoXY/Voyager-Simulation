dfbc8c8c66f1c8e1469f58badec3e7167ad32dbb
Watashiku <adelbeque@hotmail.fr>
2016-05-20 14:01:29 +0200

    Update Matrice.h

--- Matrice.h
+++ Matrice.h
@@ -8,9 +8,9 @@ class Matrice
         Matrice(int a, int b);
         virtual ~Matrice();
         double GetValue(int a,int b) { return matrix[a][b]; }
-        void GetSize(int& a, int& b) { a=nl;b=nc; }
+        void GetSize(int& a, int& b) {a=nl;b=nc;}
         void Set(double** val) { matrix = val; }
-        void SetValue(int a, int b, double x) { matrix[a][b]=x; }
+        void SetValue(int a, int b, double x) {matrix[a][b]=x;}
 
 
     protected:
