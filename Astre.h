652f97283a5890c4bd8d849f5c6a24c7abd497fa
Watashiku <adelbeque@hotmail.fr>
2016-05-20 13:58:54 +0200

    Update Astre.h

--- Astre.h
+++ Astre.h
@@ -12,16 +12,16 @@ class Astre
     public:
         Astre();
         virtual ~Astre();
-        double Getmass() const {return mass;}
-        void Setmass(double val) {mass = val;}
-        double Getradius() const {return radius;}
-        void Setradius(double val) {radius = val;}
-        double Gettheta0() {return theta0;}
-        void Settheta0(double val) {theta0 = val;}
-        Matrice* GetX()  {return &X;}
-        double GetX(int a) const {return X.GetValue(a, 0);}
-        Matrice* GetY()  {return &Y;}
-        double GetY(int a) const {return Y.GetValue(a, 0);}
+        double Getmass() { return mass; }
+        void Setmass(double val) { mass = val; }
+        double Getradius() { return radius; }
+        void Setradius(double val) { radius = val; }
+        double Gettheta0() { return theta0; }
+        void Settheta0(double val) { theta0 = val; }
+        Matrice* GetX() {return &X;}
+        double GetX(int a) {return X.GetValue(a, 0);}
+        Matrice* GetY() {return &Y;}
+        double GetY(int a) {return Y.GetValue(a, 0);}
 
         void Update(double t);
         void Definir(int numero);
@@ -30,8 +30,8 @@ class Astre
     private:
         double mass;
         double radius;
-        Matrice X;//On utilise X[0] pour la position et X[1] pour la vitesse selon Ox
-        Matrice Y;//On utilise Y[0] pour la position et Y[1] pour la vitesse selon Oy
+        Matrice X = Matrice(2,1);//On utilise X[0] pour la position et X[1] pour la vitesse selon Ox
+        Matrice Y = Matrice(2,1);//On utilise Y[0] pour la position et Y[1] pour la vitesse selon Oy
         double theta0;
         double speed;
 };
