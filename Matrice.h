7bc2897e8646b7de2113ecb869a77cb7e4dd1f7e
Watashiku <adelbeque@hotmail.fr>
2016-05-20 13:59:11 +0200

    Update Matrice.h

--- Matrice.h
+++ Matrice.h
@@ -6,15 +6,11 @@ class Matrice
 {
     public:
         Matrice(int a, int b);
-        Matrice();
-        Matrice(const Matrice &M);
-        Matrice& operator=(const Matrice &B);
         virtual ~Matrice();
-        double GetValue(int a,int b) const {return matrix[a][b];}
-        void GetSize(int& a, int& b) const {a=nl;b=nc;}
-        void Set(double** val) {matrix = val;}
+        double GetValue(int a,int b) { return matrix[a][b]; }
+        void GetSize(int& a, int& b) {a=nl;b=nc;}
+        void Set(double** val) { matrix = val; }
         void SetValue(int a, int b, double x) {matrix[a][b]=x;}
-
 
 
     protected:
@@ -24,8 +20,8 @@ class Matrice
         int nc;
 };
 
-Matrice operator*(const Matrice &A, const Matrice &B);
-Matrice operator+(const Matrice &A, const Matrice &B);
-Matrice operator-(const Matrice &A, const Matrice &B);
+Matrice operator*(Matrice A, Matrice B);
+Matrice operator+(Matrice A, Matrice B);
+Matrice operator-(Matrice A, Matrice B);
 
 #endif // MATRICE_H
